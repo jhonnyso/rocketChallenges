872f0eea6caa2cc651dbe3403137e5452957ba52
jhonnyso <jhonnysouzadasilva@gmail.com>
2021-08-24 17:37:46 -0300

    maximize button

--- scripts.js
+++ scripts.js
@@ -4,17 +4,31 @@ const cards = document.querySelectorAll('.card')
 
 for (const card of cards) {
    card.addEventListener('click', function() {
-       modalOverlay.classList.add('active')
+    const blogID = card.getAttribute('id')  
+    modalOverlay.classList.add('active')
+       modalOverlay.querySelector("iframe").src = `https://blog.rocketseat.com.br/${blogID}`;
        
    }) 
 }
 
+const modal = document.querySelector('.modal')
+
+document.querySelector(".maximize-modal").addEventListener("click", function(){
+    if (modal.classList.contains('maximize') != true){
+       modal.classList.add('maximize')
+    } else {
+        modal.classList.remove('maximize')
+    }
+})
+
 const closeModal = document.querySelector('.close-modal').addEventListener('click', function() {
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('iframe').src = ""
+    modal.classList.remove('maximize')
 })
 
-const maximizeModal = document.querySelector('.maximize-modal').addEventListener('click', function() {
-    modalOverlay.classList.remove('maximize')
-})
 
-modalOverlay.querySelector("iframe").src = "https://blog.rocketseat.com.br/es07-novas-features-no-javascript/"
+
+
+
+
